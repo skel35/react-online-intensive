787b0663a026ef27054433f19cf2a81a3625af9a
Taras Petruk <taras.petruk@gmail.com>
2018-11-28 00:10:27 +0200

    after lesson 6

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,5 +1,6 @@
 // Core
 import React, { Component } from 'react';
+import moment from 'moment';
 
 // Components
 import Post from 'components/Post';
@@ -9,11 +10,88 @@ import Spinner from 'components/Spinner';
 
 // Instruments
 import Styles from './styles.m.css';
+import { getUniqueID, delay } from 'instruments';
 
 export default class Feed extends Component {
+    constructor() {
+        super();
+        this._createPost = this._createPost.bind(this);
+        this._likePost = this._likePost.bind(this);
+        this._deletePost = this._deletePost.bind(this);
+        this._setPostsFetchingState = this._setPostsFetchingState.bind(this);
+    }
+
     state = {
-        posts:           [{ id: '123', comment: 'Hi there!', created: 1543347855 }, { id: '456', comment: 'Приветик!', created: 1543347873 }],
-        isPostsFetching: true,
+        posts:           [{ id: '123', comment: 'Hi there!', created: 1543347855, likes: [] }, { id: '456', comment: 'Приветик!', created: 1543347873, likes: [] }],
+        isPostsFetching: false,
+    }
+
+    _setPostsFetchingState(isFetching) {
+        this.setState({
+            isPostsFetching: isFetching,
+        });
+    }
+
+    async _createPost(comment) {
+        this._setPostsFetchingState(true);
+        const post = {
+            id:      getUniqueID(),
+            created: moment().unix(),
+            comment,
+            likes:   [],
+        };
+        await delay(1200);
+        this.setState({
+            isPostsFetching: false,
+        });
+        this.setState(({posts}) => ({
+            posts:           [ post, ...posts ],
+            isPostsFetching: false,
+        }));
+    }
+
+    async _deletePost(id) {
+        this._setPostsFetchingState(true);
+        await delay(1200);
+        this.setState(({posts}) => {
+            const newPosts = posts.filter((post) => post.id !== id);
+
+            return {
+                posts:           newPosts,
+                isPostsFetching: false,
+            };
+        });
+    }
+
+    async _likePost(id) {
+        const { currentUserFirstName, currentUserLastName } = this.props;
+        this._setPostsFetchingState(true);
+
+        await delay(1200);
+
+        this.setState((oldState) => {
+            const newPosts = oldState.posts.map((post) => {
+                if (post.id === id) {
+                    return {
+                        ...post,
+                        likes: [
+                            {
+                                id:        getUniqueID(),
+                                firstName: currentUserFirstName,
+                                lastName:  currentUserLastName,
+                            },
+                        ],
+                    };
+                }
+
+                return post;
+            });
+
+            return {
+                posts:           newPosts,
+                isPostsFetching: false,
+            };
+        });
     }
 
     render() {
@@ -22,6 +100,8 @@ export default class Feed extends Component {
         const postsJSX = posts.map((post) => {
             return (
                 <Post
+                    _deletePost = { this._deletePost }
+                    _likePost = { this._likePost }
                     key = { post.id }
                     { ...post }
                 />
@@ -32,7 +112,7 @@ export default class Feed extends Component {
             <section className = { Styles.feed }>
                 <Spinner isSpinning = { isPostsFetching } />
                 <StatusBar />
-                <Composer />
+                <Composer _createPost = { this._createPost } />
                 { postsJSX }
             </section>
         );
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,31 +1,55 @@
 // Core
 import React, { Component } from 'react';
 import moment from 'moment';
-import PropTypes from 'prop-types';
+import { array, func, number, string } from 'prop-types';
 
 // Components
 import { Consumer } from 'components/HOC/withProfile';
+import Like from 'components/Like';
 
 // Instruments
 import Styles from './styles.m.css';
 
 export default class Post extends Component {
     static propTypes = {
-        comment: PropTypes.string.isRequired,
-        created: PropTypes.number.isRequired,
+        comment:     string.isRequired,
+        created:     number.isRequired,
+        _likePost:   func.isRequired,
+        _deletePost: func.isRequired,
+        likes:       array.isRequired,
+        id:          string.isRequired,
     };
 
+    constructor() {
+        super();
+        this._deletePost = this._deletePost.bind(this);
+    }
+
+    _deletePost() {
+        this.props._deletePost(this.props.id);
+    }
+
     render () {
-        const { comment, created } = this.props;
+        const { comment, created, _likePost, likes, id } = this.props;
 
         return (
             <Consumer>
                 {(context) => (
                     <section className = { Styles.post }>
+                        <span
+                            className = { Styles.cross }
+                            onClick = { this._deletePost }
+                        />
                         <img src = { context.avatar } />
                         <a>{`${context.currentUserFirstName} ${context.currentUserLastName}`}</a>
                         <time>{moment.unix(created).format('MMMM D h:mm:ss a')}</time>
                         <p>{comment}</p>
+                        <Like
+                            _likePost = { _likePost }
+                            id = { id }
+                            likes = { likes }
+                            { ...context }
+                        />
                     </section>
                 )}
             </Consumer>
